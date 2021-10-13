ca66303fd6e1eecdc35b086bcaf8360a921f94d5
Amohamed96 <amohamed.rg@gmail.com>
2021-10-13 11:16:43 -0400

    Chorus

--- chorus-for.js
+++ chorus-for.js
@@ -1,5 +1,2 @@
-const chorus = "Let's dance!";
-for (let repeat = 0; repeat < 10; repeat++) {
-  console.log(chorus);
-}
-console.log("Until the sun comes up!");
+result = Math.sign(10);
+console.log(result);
